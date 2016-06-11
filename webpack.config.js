b593b1be88736db4dbc72f10f42c4f2545c95d37
Zach <zach.bay@gmail.com>
2016-06-10 18:19:02 -0700

    Initial useless todo app

--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,18 @@ module.exports = {
   ],
   output: {
     path: __dirname,
-    filename:'./public/bundle.js'
+    filename: './public/bundle.js'
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components'
+    ],
     alias: {
       applicationStyles: 'app/styles/app.scss'
     },
-    extentions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [
