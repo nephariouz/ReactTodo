28ac1a4c9e9f09f842382338f96a10d2b89d0810
Zach <zach.bay@gmail.com>
2016-06-29 16:59:04 -0700

    Style Todo app

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -8,21 +8,29 @@ var TodoList = require('TodoList');
 var Todo = require('Todo');
 
 describe('TodoList', () => {
-	it('should exist', () => {
-		expect(TodoList).toExist();
-	});
+  it('should exist', () => {
+    expect(TodoList).toExist();
+  });
 
-	it('should render one Todo component for each todo item', () => {
-		var todos = [{
-			id: 1,
-			text: 'Do something'
-		}, {
-			id: 2,
-			text: 'Check mail'
-		}];
-		var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-		var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+  it('should render one Todo component for each todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Do something'
+    }, {
+      id: 2,
+      text: 'Check mail'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
 
-		expect(todosComponents.length).toBe(todos.length);
-	});
+    expect(todosComponents.length).toBe(todos.length);
+  });
+
+  it('should render empty message if no todos', () => {
+    var todos = [];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var $el = $(ReactDOM.findDOMNode(todoList));
+
+    expect($el.find('.container__message').length).toBe(1);
+  });
 });
